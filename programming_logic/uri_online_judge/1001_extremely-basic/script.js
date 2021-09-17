6c66d764879a1649b5abc57e192c3fc5e158e0f3
JesseLopesDev <jesselopes.dev@gmail.com>
2021-09-17 19:53:35 -0300

    Solving the Extremely Basic challenge

---
+++ programming_logic/uri_online_judge/1001_extremely-basic/script.js
@@ -0,0 +1,16 @@
+const input = require('fs').readFileSync('./dev/stdin', 'utf-8');
+const samples = input.split('\n');
+
+const values = [];
+
+function calculateValues() {
+  for (let index = 0; index <= 2; index++) {
+    values[index] = parseInt(samples.shift());
+  }
+
+  return values[0] + values[1];
+}
+
+const sum = calculateValues();
+
+console.log(`X = ${sum}`);
